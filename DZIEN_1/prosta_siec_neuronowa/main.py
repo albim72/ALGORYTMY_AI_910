0e8b836ffb0d29cd4af9161a895761901fd29e0c
albim72 <marcin.albiniak72@gmail.com>
2025-09-29 14:14:26 +0200

    Create main.py

---
+++ DZIEN_1/prosta_siec_neuronowa/main.py
@@ -0,0 +1,12 @@
+port numpy as np
+
+class SimpleNeuralNetwork:
+    # def __new__(cls):
+    #     return super().__new__(cls)
+
+    def __init__(self):
+        np.random.seed(1)
+        self.weights = 2*np.random.rand(3, 1)-1
+
+    def __repr__(self):
+        return f"SimpleNeuralNetwork(weights=\n{self.weights})"
