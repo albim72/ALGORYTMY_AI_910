5e0bd316fab22ebfc9f1ff51c62e33d46ecad520
albim72 <marcin.albiniak72@gmail.com>
2025-09-29 15:07:16 +0200

    Update main.py

--- DZIEN_1/prosta_siec_neuronowa/main.py
+++ DZIEN_1/prosta_siec_neuronowa/main.py
@@ -1,12 +1,27 @@
-port numpy as np
+import numpy as np
+from simplenn import SimpleNeuralNetwork
 
-class SimpleNeuralNetwork:
-    # def __new__(cls):
-    #     return super().__new__(cls)
+network  = SimpleNeuralNetwork()
+print(network)
 
-    def __init__(self):
-        np.random.seed(1)
-        self.weights = 2*np.random.rand(3, 1)-1
+#dane wejściowe
+train_inputs = np.array([[1,1,0],[1,1,1],[1,1,0],[1,0,0],[0,1,1],[0,1,0],[0,0,0]])
+train_outputs = np.array([[1,0,1,1,0,1,0]]).T
+train_iters = 50_000
 
-    def __repr__(self):
-        return f"SimpleNeuralNetwork(weights=\n{self.weights})"
+#trening sieci neuronowej
+network.train(train_inputs,train_outputs,train_iters)
+print(f"\nwagi po treningu:\n{network.weights}")
+
+#testowanie
+print(f"\nprzykładowe wejście: {train_inputs[0]}")
+print(f"\nprzykładowe wyjście: {train_outputs[0]}")
+print(f"\nwyjście po wykonaniu: {network.propagation(train_inputs[0])} \n\n")
+
+#predykcja
+test_data = np.array([[1,1,1],[1,0,0],[0,1,1],[0,1,0],[0,0,1],[0,0,0]])
+print(f"\npredykcja z wykorzystamniem sieci neuronowej")
+for data in test_data:
+    print(f"przykładowe dane: {data}")
+    print(f"wyjście predykcji: {network.propagation(data)}\n\n")
+
